4c0eaae28c41281b14aa3900104919f8405d56ef
jackinabox86 <cyrusa86@gmail.com>
2026-04-22 21:52:44 -0400

    Revert "XIT BS: add Prod column"

--- src/features/XIT/BS/BS.ts
+++
@@ -1,8 +0,0 @@
-import BS from '@src/features/XIT/BS/BS.vue';
-
-xit.add({
-  command: 'BS',
-  name: 'BASE OVERVIEW',
-  description: 'Lists all player bases with links to key base commands.',
-  component: () => BS,
-});
